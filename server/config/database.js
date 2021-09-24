18b6a5be7dbf55b8c2a873c0ff6fbca4d7afc8a0
Cybre3 <71160954+Cybre3@users.noreply.github.com>
2021-09-24 01:08:36 -0400

    edited the server to console log successful connection and errors

--- server/config/database.js
+++ server/config/database.js
@@ -1,14 +1,17 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-mongoose.set('useCreateIndex', true); // Removes error "DeprecationWarning: collection.ensureIndex is deprecated. Use createIndexes instead."
+mongoose.set("useCreateIndex", true); // Removes error "DeprecationWarning: collection.ensureIndex is deprecated. Use createIndexes instead."
 
 module.exports = () => {
-    return mongoose.connect("mongodb+srv://@cluster0.xwf5v.mongodb.net/?retryWrites=true&w=majority", {
-        dbName: process.env.DATABASE_NAME,
-        user: process.env.DB_USER,
-        pass: process.env.DB_PASS,
-        useNewUrlParser: true, 
-        useUnifiedTopology: true
-    });
+  return mongoose
+    .connect("mongodb+srv://@cluster0.xwf5v.mongodb.net/?retryWrites=true&w=majority", {
+      dbName: process.env.DATABASE_NAME,
+      user: process.env.DB_USER,
+      pass: process.env.DB_PASS,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => console.log("Connected to db!"))
+    .catch((error) => console.log("db connection failed: " + error));
 };
