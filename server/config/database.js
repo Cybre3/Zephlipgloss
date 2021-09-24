3b428150bb553dbdb6ad0e347f78cfc41aec0704
marissao <50351548+marissao@users.noreply.github.com>
2021-09-23 16:39:20 -1000

    Connected server

--- server/config/database.js
+++ server/config/database.js
@@ -1,6 +1,14 @@
 const mongoose = require('mongoose');
-const config = require('./config');
+require('dotenv').config();
+
+mongoose.set('useCreateIndex', true); // Removes error "DeprecationWarning: collection.ensureIndex is deprecated. Use createIndexes instead."
 
 module.exports = () => {
-    return mongoose.connect(config.dbURL, { useNewUrlParser: true, useUnifiedTopology: true });
+    return mongoose.connect("mongodb+srv://@cluster0.xwf5v.mongodb.net/?retryWrites=true&w=majority", {
+        dbName: process.env.DATABASE_NAME,
+        user: process.env.DB_USER,
+        pass: process.env.DB_PASS,
+        useNewUrlParser: true, 
+        useUnifiedTopology: true
+    });
 };
