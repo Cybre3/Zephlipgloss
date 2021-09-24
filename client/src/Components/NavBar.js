4a75906e88e60f7ca3070fc695877022a63eaa69
Cybre3 <71160954+Cybre3@users.noreply.github.com>
2021-09-23 23:20:50 -0400

    backing up navbar

--- client/src/Components/NavBar.js
+++ client/src/Components/NavBar.js
@@ -28,3 +28,38 @@ const NavBar = () => {
 };
 
 export default NavBar;
+
+// import React from "react";
+// import { Link } from "react-router-dom";
+
+// const NavBar = () => {
+//   return (
+//     <div className="p-3 mb-2 bg-gradient-primary text-white">.bg-gradient-primary</div>
+//     /* <div>
+//       <nav className="navbar navbar-expand-lg navbar-light bg-light">
+//       <a class="navbar-brand" href="#">Image</a>
+//         <div className="collapse navbar-collapse justify-content-end">
+//           <ul className="navbar-nav">
+//             <li className="nav-item active">
+//               <a className="nav-link" href="/">Home</a>
+//             </li>
+//             <li className="nav-item">
+//               <a className="nav-link" href="/shop">shop</a>
+//             </li>
+//             <li className="nav-item">
+//               <a className="nav-link" href="/sale">SALE</a>
+//             </li>
+//             <li className="nav-item">
+//               <a className="nav-link" href="/instagram">Instagram</a>
+//             </li>
+//             <li className="nav-item">
+//               <a class="nav-link dropdown-toggle" href="/explore">Explore</a>
+//             </li>
+//           </ul>
+//         </div>
+//       </nav>
+//     </div> */
+//   );
+// };
+
+// export default NavBar;
