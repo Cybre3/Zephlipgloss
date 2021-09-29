9e6d537aad4c5cf4739dce6db9eacbb78de9e240
Cybre3 <71160954+Cybre3@users.noreply.github.com>
2021-09-28 20:56:32 -0400

    NavIcon component finished

--- client/src/Components/SubComponents/NavIcon.js
+++ client/src/Components/SubComponents/NavIcon.js
@@ -1,9 +1,11 @@
-const NavIcon = () => {
-  return ( 
-    <div>
-      <p>placeholder</p>
+const NavIcon = (props) => {
+  return (
+    <div className="mt-2">
+      <a href={props.linkTo} class="me-4 text-reset">
+        <i className={props.class}></i>
+      </a>
     </div>
-   );
-}
- 
+  );
+};
+
 export default NavIcon;
