912c21fd67270279f656054b15d21e5926ae8f57
marissao <50351548+marissao@users.noreply.github.com>
2021-10-07 07:21:08 -1000

    Details page is not loading any data from useFetch. Tried to use conditionals or useState w/ obj returned from useFetch but JSX elements output as empty

--- client/src/Components/Product-Details.js
+++ client/src/Components/Product-Details.js
@@ -1,6 +1,8 @@
 import PinkButton from "./SubComponents/Button";
 import useFetch from "./SubComponents/useFetch";
 import { useParams } from "react-router";
+import { useState } from "react";
+
 const ProductDetails = (props) => {
   // let product = {
   //     _id: '1',
@@ -21,56 +23,61 @@ const ProductDetails = (props) => {
   const { id } = useParams();
   const { data: product } = useFetch("http://localhost:5000/product/" + id);
   const { description } = product;
-  console.log(product);
-  console.log(id);
+  const [data, setData] = useState(product);
+  console.log("This is product: ", product.length);
+  console.log("This is id: ", id);
   return (
     <div className="container">
-      <div className="row">
-        <a href="/">All Products</a>
-      </div>
-      <div className="row">
-        <div className="col-sm">
-          <div>
-            <img className="product-img" src={product.img} alt={`lipgloss-${product.title}`} />
-          </div>
+        <div className="row">
+            <a href="/">All Products</a>
         </div>
-        {/* Add carousel here later */}
-        <div className="col-sm">
-          <h3 className="product-name-details-pg">{product.name}</h3>
-          <p className="product-price-details-pg">${product.price}</p>
-          <p className="product-instock">{product.inventory > 0 ? "In Stock" : "Out of Stock"}</p>
-          <p className="select-type-lable">
-            COLOR{" "}
-            <select>
-              {/* <option value={product.color[0]}>{product.color[0]}</option>
-              <option value={product.color[1]}>{product.color[1]}</option> */}
-            </select>
-          </p>
-          <p className="select-type-lable">
-            TUBE{" "}
-            <select>
-              {/* <option value={product.tube[0]}>{product.tube[0]}</option>
-              <option value={product.tube[1]}>{product.tube[1]}</option>
-              <option value={product.tube[2]}>{product.tube[2]}</option> */}
-            </select>
-          </p>
-          <p className="select-type-lable">
-            Quantity{" "}
-            <select>
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-            </select>
-          </p>
-          <p className="description-text">{product.description["summary"]}</p>
-          {/* 
-          <p className="description-text">{product.description.notifications}</p>
-          <p className="description-text">{product.description.ingredients}</p> */}
-          <PinkButton action="Add to Cart" />
-        </div>
-      </div>
+        
+        { product.length > 0 && 
+            <div className="row">
+            <div className="col-sm">
+                <div>
+                    <img className="product-img" src={product.img} alt={`lipgloss-${product.title}`} />
+                </div>
+            </div>
+            {/* Add carousel here later */}
+            <div className="col-sm">
+                <h3 className="product-name-details-pg">{product.name}</h3>
+                <p className="product-price-details-pg">${product.price}</p>
+                <p className="product-instock">{product.inventory > 0 ? "In Stock" : "Out of Stock"}</p>
+                <p className="select-type-lable">
+                    COLOR{" "}
+                    <select>
+                    {/* <option value={product.color[0]}>{product.color[0]}</option>
+                    <option value={product.color[1]}>{product.color[1]}</option> */}
+                    </select>
+                </p>
+                <p className="select-type-lable">
+                    TUBE{" "}
+                    <select>
+                    {/* <option value={product.tube[0]}>{product.tube[0]}</option>
+                    <option value={product.tube[1]}>{product.tube[1]}</option>
+                    <option value={product.tube[2]}>{product.tube[2]}</option> */}
+                    </select>
+                </p>
+                <p className="select-type-lable">
+                    Quantity{" "}
+                    <select>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    </select>
+                </p>
+                {/* <p className="description-text">{product.description.summary}</p> */}
+                {/* 
+                <p className="description-text">{product.description.notifications}</p>
+                <p className="description-text">{product.description.ingredients}</p> */}
+                <PinkButton action="Add to Cart" />
+            </div>
+        </div> 
+        }
+
     </div>
   );
 };
