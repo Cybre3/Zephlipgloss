2f82eada07a2f13223c9789aad711faab4d27a4c
Cybre3 <71160954+Cybre3@users.noreply.github.com>
2021-10-07 22:23:13 -0400

    added save user route controller template

---
+++ server/controllers/user.js
@@ -0,0 +1,18 @@
+const dbo = require("../db/conn");
+const ObjectId = require("mongodb").ObjectId;
+
+module.exports = {
+  get: {
+    save: (req, res) => {
+      let db_connect = dbo.getDb();
+      db_connect
+        .collection("products")
+        .find({})
+        .toArray(function (err, result) {
+          if (err) throw err;
+          res.json(result);
+        });
+    }
+  },
+  post: {},
+};
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,16 +1,5 @@
 const express = require("express");
 const recordRoutes = express.Router();
-const dbo = require("../db/conn");
-const ObjectId = require("mongodb").ObjectId;
 
 // save user
-recordRoutes.route("/register").post(function (req, res) {
-  let db_connect = dbo.getDb();
-  db_connect
-    .collection("products")
-    .find({})
-    .toArray(function (err, result) {
-      if (err) throw err;
-      res.json(result);
-    });
-});
+recordRoutes.route("/register").post();
