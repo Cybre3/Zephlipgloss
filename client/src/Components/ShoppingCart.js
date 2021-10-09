e5dea42d7b1fa77a9021145ce7f96e72e44fc2ce
Cybre3 <71160954+Cybre3@users.noreply.github.com>
2021-10-09 18:31:42 -0400

    started shopping cart

--- client/src/Components/ShoppingCart.js
+++ client/src/Components/ShoppingCart.js
@@ -1,22 +1,31 @@
 import PinkButton from "./SubComponents/Button";
 import CartItem from "./SubComponents/CartItem";
+import React from "react";
+import { Formik, Form } from "formik";
+import * as Yup from "yup";
+import FormikControl from "./Form/FormikControl";
+import ProductDetails from "./Product-Details";
+import useFetch from "./useFetch";
 
-const ShoppingCart = () => {
-    return (
-        <div className="shopping-cart">
-            <div className="cart-screen-left">
-                <h2>Shopping Cart</h2>
-                <CartItem />
-            </div>
-            <div className="cart-screen-right">
-                <div className="cart-screen-info">
-                    <p>Subtotal (1) Items</p>
-                    <p>$10.00</p>
-                </div>
-                <PinkButton action="Checkout"/>
-            </div>
-        </div>
-    );
+const ShoppingCart = (props) => {
+    const { data: allCartProducts } = useFetch("http://localhost:5000/shopping-cart");
+    <ProductDetails products={allCartProducts} />
+
+      // return (
+    //     <div className="shopping-cart">
+    //         <div className="cart-screen-left">
+    //             <h2>Shopping Cart</h2>
+    //             <CartItem />
+    //         </div>
+    //         <div className="cart-screen-right">
+    //             <div className="cart-screen-info">
+    //                 <p>Subtotal (1) Items</p>
+    //                 <p>$10.00</p>
+    //             </div>
+    //             <PinkButton action="Checkout"/>
+    //         </div>
+    //     </div>
+    // );
 }
  
 export default ShoppingCart;
